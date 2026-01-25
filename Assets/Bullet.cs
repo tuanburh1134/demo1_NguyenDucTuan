7cc805eebbdf5661e27a67a3dd8d166ceef0915f
tuanburh1134 <tuan75035@gmail.com>
2026-01-25 21:57:07 +0700

    demo2 Nguyen Duc Tuan

---
+++ Assets/Bullet.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    public float flySpeed; // Tốc độ bay của đạn
+
+    // Update is called once per frame
+    void Update()
+    {
+        var newPosition = transform.position;
+        newPosition.y += Time.deltaTime * flySpeed; // Thay đổi vị trí theo trục Y
+        transform.position = newPosition;
+    }
+}
